6f68f523bc92800bf3000c06f6b07333ab8e97a5
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-28 12:43:51 +0300

    location model

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.name
