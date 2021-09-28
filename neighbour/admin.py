f0d86d592eacf47725318c728ffac0e631af87c3
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-28 12:50:54 +0300

    registered my models in admin

--- neighbour/admin.py
+++ neighbour/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post,Comment,Neighbourhood,Business,Profile,Location,Category
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(Category)
