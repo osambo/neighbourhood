10d35d03f3e289e147c97d5a3e17c4db21fdda55
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-28 12:34:01 +0300

    created my hood database

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -13,19 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-from django.conf.urls import url,include
 from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url,include
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'',include('neighbourhood.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    path('^admin/', admin.site.urls),
+    path('',include('neighbourhood.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/$', views.logout, {"next_page": '/'}), 
 ]
