e56e2b86ab35de8f9f98636fcefc7514d8dae949
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-28 12:44:38 +0300

    neigborhood model

--- neighbour/models.py
+++ neighbour/models.py
@@ -13,3 +13,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Neighbourhood(models.Model):
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE,null = True)
+    occupants = models.IntegerField(default=0)
+
+    def save_neighbourhood(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def update_neighbourhood(self):
+        self.save()
+
+    def update_occupants(self):
+        self.occupants += 1
+        self.save()
