555807ad91a7b679a659d047457dc33e3d025460
kodhanjo <felix.osambo@student.moringaschool.com>
2021-09-28 13:20:56 +0300

    frrezed dependencies

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'neighbourhood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,8 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    
